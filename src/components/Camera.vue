1d4282b8dcc327a51e5eaf6dd15f5cb46b9f0ae8
周涛 <zhoutao@uniontech3d.cn>
2019-10-13 20:27:46 +0800

    :sparkles: 新增2019年素材

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -2,6 +2,43 @@
 
     <div class="carame">
         <Collapse v-model="defaultShowPanel" accordion>
+            <Panel class="Panel" name="大事件时间轴">
+                大事件时间轴
+                <div slot="content" style="overflow-y: auto;height: 300px">
+                    <Timeline>
+                        <TimelineItem color="green">
+                            <p class="time">2011年12月</p>
+                            <p>🔥 歃血为盟,结兄弟谊；死生相托,吉凶相救；福祸相依,患难相扶;</p>
+                        </TimelineItem>
+
+                        <TimelineItem color="red">
+                            <p class="time">2012年4月日</p>
+                            <p>我们从涡阳四中,各奔前程</p>
+                        </TimelineItem>
+
+
+                        <TimelineItem color="red">
+                            <p class="time">2018年年10月1日</p>
+                            <p>xxxxxx</p>
+                        </TimelineItem>
+
+
+                        <TimelineItem color="red">
+                            <p class="time">2012年6月7日</p>
+                            <p>我们从涡阳四中,各奔前程</p>
+                        </TimelineItem>
+
+
+                        <TimelineItem color="red">
+                            <p class="time">2012年6月7日</p>
+                            <p>我们从涡阳四中,各奔前程</p>
+                        </TimelineItem>
+
+                    </Timeline>
+                </div>
+            </Panel>
+
+
             <Panel class="Panel" :name="name" v-for="(serial,name) of serialList">
                 {{name}}
                 <p slot="content" class="content">
@@ -21,41 +58,41 @@
     export default {
         data() {
             return {
-                defaultShowPanel: "2019年合影留念",
+                defaultShowPanel: "大事件时间轴",
                 serialList: {
-                    "2019年合影留念": [
-                        {name: "荷叶", img: pic1},
-                        {name: "荷叶", img: pic1},
-                        {name: "荷叶", img: pic1},
-                        {name: "荷叶", img: pic1},
-                        {name: "荷叶", img: pic1},
-                        {name: "荷叶", img: pic1},
-                        {name: "荷叶", img: pic1},
-                        {name: "荷叶", img: pic1},
-                        {name: "荷叶", img: pic1},
+                    "2015年合影留念": [
+                        {name: "荷叶", img: "http://pzb5gmte2.bkt.clouddn.com/2015"},
+                        {name: "荷叶", img: "http://pzb5gmte2.bkt.clouddn.com/2015.2"},
+                    ],
+                    "2016年合影留念": [
+                        {name: "荷叶", img: "http://pzb5gmte2.bkt.clouddn.com/2016"},
                     ],
                     "2018年合影留念": [
-                        {name: "荷叶222", img: pic2},
-                        {name: "荷叶222", img: pic2},
-                        {name: "荷叶222", img: pic2},
-                        {name: "荷叶222", img: pic2},
-                        {name: "荷叶222", img: pic2},
-                        {name: "荷叶222", img: pic2},
-                        {name: "荷叶222", img: pic2}
+                        {name: "荷叶222", img: "http://pzb5gmte2.bkt.clouddn.com/2018"},
+                        {name: "荷叶222", img: "http://pzb5gmte2.bkt.clouddn.com/2018.2"},
+                        {name: "荷叶222", img: "http://pzb5gmte2.bkt.clouddn.com/2018.3"},
+                    ], "2019年合影留念": [
+                        {name: "回廊", img: "http://pzb5gmte2.bkt.clouddn.com/2019.1.jpg"},
+                        {name: "荷叶", img: "http://pzb5gmte2.bkt.clouddn.com/2019.4.jpg"},
+                        {name: "荷叶2", img: "http://pzb5gmte2.bkt.clouddn.com/2019.5.back.jpg"},
+                        {name: "池边", img: "http://pzb5gmte2.bkt.clouddn.com/2019.2"},
+                        {name: "池边2", img: "http://pzb5gmte2.bkt.clouddn.com/2019.3"},
+
                     ]
                 }
             }
         }, methods: {
             showPic: function (pic) {
                 this.$Modal.confirm({
-                    title: pic.name || "暂无描述",
-                    content: '<img src="' + pic.img + '" style="width:1080px;height:607px">',
-                    width: '1200px',
+                    title: pic.name + "详情信息",
+                    okText: "全屏浏览",
+                    cancelText: "关闭",
+                    content: '<img class="modelImg" src="' + pic.img + '" style="width:100%;height:100%">',
                     onOk: () => {
-                        this.$Message.info('Clicked ok');
+                        window.open(pic.img, '_blank');
                     },
                     onCancel: () => {
-                        this.$Message.info('Clicked cancel');
+
                     }
                 });
             }
@@ -76,6 +113,16 @@
         display: flex;
         float: left;
         border-radius: 5px;
+        box-shadow: 1px 1px 5px 1px #2c3e50;
+
+    }
+
+    .modelImg {
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
     }
 
     .content {
@@ -83,5 +130,31 @@
         overflow-x: auto;
         height: 500px;
     }
+
+    .demo {
+        display: inline-block;
+        width: 600px;
+        height: 338px;
+        text-align: center;
+        line-height: 100px;
+        border: 1px solid transparent;
+        border-radius: 4px;
+        overflow: hidden;
+        background: #fff;
+        position: relative;
+        box-shadow: 0 1px 1px rgba(0, 0, 0, .2);
+        margin-right: 4px;
+    }
+
+    .demo:hover {
+        display: block;
+
+    }
+
+    .video-player {
+        width: 270px;
+        height: 460px;
+    }
+
 
 </style>
