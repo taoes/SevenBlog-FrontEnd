29fe37bcf542c21abb054ca7dd1c9031a79d1b19
周涛 <zhoutao@uniontech3d.cn>
2019-10-13 16:56:25 +0800

    tada: 新增饼图设置

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -30,26 +30,22 @@
 
         <Row style="padding:20px">
             <Col span="12">
-                <Card :bordered="false">
+                <Card :bordered="false" style="height:400px">
                     <div slot="title">
-                        <Icon type="logo-github" size="20"/>
+                        <Icon type="md-pie" size="20"/>
                         <span class="cardTitle">文章内容</span>
                     </div>
-                    <p>Content of card</p>
-                    <p>Content of card</p>
-                    <p>Content of card</p>
+                    <ve-pie :data="contentRateData" :settings="setting"></ve-pie>
                 </Card>
             </Col>
 
             <Col span="12">
-                <Card :bordered="true">
+                <Card :bordered="true" style="height: 400px">
                     <div slot="title">
-                        <Icon type="logo-wordpress" size="20"/>
+                        <Icon type="ios-map" size="20"/>
                         <span class="cardTitle">访客信息</span>
                     </div>
-                    <p>Content of card</p>
-                    <p>Content of card</p>
-                    <p>Content of card</p>
+                    <ve-ring :data="accessData" :settings="setting"></ve-ring>
                 </Card>
             </Col>
         </Row>
@@ -79,7 +75,39 @@
     export default {
         data() {
             return {
-                data: []
+                data: [],
+                setting: {
+                    radius: 100,
+                    label: {
+                        align: 'left',
+                        verticalAlign: 'top'
+                    },
+                },
+                contentRateData: {
+                    columns: ['日期', '访问用户'],
+                    rows: [
+                        {'日期': 'JVM虚拟机', '访问用户': 1393},
+                        {'日期': 'React', '访问用户': 3530},
+                        {'日期': 'VUE', '访问用户': 2923},
+                        {'日期': 'NodeJS', '访问用户': 1723},
+                        {'日期': 'GoLang', '访问用户': 3792},
+                        {'日期': 'Python', '访问用户': 4593}
+                    ]
+                }, accessData: {
+                    columns: ['日期', '访问用户'],
+                    radius: 1,
+                    label: {
+                        align: 'left',
+                        verticalAlign: 'top'
+                    },
+                    rows: [
+                        {'日期': '🇨🇳 中国', '访问用户': 1390},
+                        {'日期': '🇯🇵 日本', '访问用户': 353},
+                        {'日期': '🇰🇷 韩国', '访问用户': 292},
+                        {'日期': '🇺🇸 美国', '访问用户': 17},
+                        {'日期': '🏳️‍🌈其他', '访问用户': 37},
+                    ]
+                }
             }
         }, methods: {}
     }
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import store from './store/Store.js'
 import router from './router/Router'
 import VueRouter from 'vue-router'
 import ViewUI from 'view-design';
+import VCharts from 'v-charts'
 import 'view-design/dist/styles/iview.css';
 
 Vue.use(ViewUI);
+Vue.use(VCharts);
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
