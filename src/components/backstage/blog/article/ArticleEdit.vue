ee592cf94fe0f7fa44c0cf9119e2a637c2816ae5
tangsijun <tsj_online@163.com>
2019-10-24 19:24:09 +0800

    样式

--- src/components/backstage/blog/article/ArticleEdit.vue
+++ src/components/backstage/blog/article/ArticleEdit.vue
@@ -1,30 +1,28 @@
 <template>
-    <div class="articleEdit">
-        <el-input class="inputTitle" style="width: 60%"></el-input>
-        <mavon-editor
-                codeStyle="vs2015"
-                v-model="article.content"
-                style="max-height: 960px;width: 100%;margin-right: 40px"></mavon-editor>
+  <div class="articleEdit">
+    <el-input style="width:60%" />
+    <div class="mavon">
+      <mavon-editor codeStyle="vs2015" v-model="article.content" class="editor" />
     </div>
+  </div>
 </template>
 
 <script>
-    import './ArticleEdit.css'
-    import {mavonEditor} from 'mavon-editor'
+import "./ArticleEdit.css";
+import { mavonEditor } from "mavon-editor";
 
-    export default {
-        name: "ArticleEdit",
-        data: function () {
-            return {
-                article: {title: '34'}
-            }
-        },
-        components: {mavonEditor},
-        methods: {},
-        watch: {}
-    }
+export default {
+  name: "ArticleEdit",
+  data: function() {
+    return {
+      article: { title: "34" }
+    };
+  },
+  components: { mavonEditor },
+  methods: {},
+  watch: {}
+};
 </script>
 
 <style scoped>
-
 </style>
