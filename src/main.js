2fe9ef14feb6e697561b6fc13cb460d0fd062d39
周涛 <zhoutao@uniontech3d.cn>
2019-11-07 20:14:09 +0800

    :bug: 修复bug

--- src/main.js
+++ src/main.js
@@ -22,19 +22,18 @@ import solid from '@fortawesome/fontawesome-free-solid'
 import regular from '@fortawesome/fontawesome-free-regular'
 import brands from '@fortawesome/fontawesome-free-brands'
 
-fontawesome.library.add(solid)
-fontawesome.library.add(regular)
-fontawesome.library.add(brands)
+fontawesome.library.add(solid);
+fontawesome.library.add(regular);
+fontawesome.library.add(brands);
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(mavonEditor);
 Vue.use(ElementUI);
 Vue.use(VCharts);
 // 引入
-import MarkdownRun from 'vue-markdown-run';
+
 // 全局注入
-Vue.use(MarkdownRun);
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 Vue.use(Viewer)
