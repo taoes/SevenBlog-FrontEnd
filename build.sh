741372fc7229b33fca2374218c34776303978bd6
周涛 <zhoutao825638@vip.qq.com>
2020-10-31 14:00:57 +0800

    :pencil: 新增编译脚本

---
+++ build.sh
@@ -0,0 +1,19 @@
+yarn build
+rm -rf ./build
+rm ./build.tar.gz
+
+
+mkdir -p ./build
+cp -rf ./.nuxt ./build
+cp -rf ./static ./build
+cp -rf ./assets ./build
+cp ./package.json ./build
+cp ./nuxt.config.js ./build
+
+
+tar -czvf ./build.tar.gz ./build
+scp ./build.tar.gz root@47.98.143.247:/blog-fe
+
+
+rm -rf ./build
+rm ./build.tar.gz
