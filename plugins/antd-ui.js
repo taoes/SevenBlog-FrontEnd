06e9a6490b7ac6fa02d44abecc689298a1034b86
周涛 <zhoutao825638@vip.qq.com>
2020-12-19 15:23:36 +0800

    :bug: 修复提交的bug

--- plugins/antd-ui.js
+++ plugins/antd-ui.js
@@ -8,6 +8,7 @@ import Avatar from 'ant-design-vue/lib/avatar'
 import List from 'ant-design-vue/lib/list'
 import Comment from 'ant-design-vue/lib/comment'
 import Button from 'ant-design-vue/lib/button'
+import Modal from 'ant-design-vue/lib/modal'
 import FormModel from 'ant-design-vue/lib/form-model'
 import Input from 'ant-design-vue/lib/input'
 import Rate from 'ant-design-vue/lib/rate'
@@ -27,6 +28,7 @@ Vue.use(Avatar);
 Vue.use(List);
 Vue.use(Comment);
 Vue.use(Button);
+Vue.use(Modal);
 Vue.use(FormModel);
 Vue.use(Input);
 Vue.use(Rate);
